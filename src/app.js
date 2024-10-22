9c86397e7eba590d0cba158b8cc5c0597d571736
ajit <ajeets@1028.com>
2024-10-22 17:53:54 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,22 +2,9 @@ const express = require("express");
 
 const app = express();
 
-//This will only handle GET call to /user
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "Ajit", lastName: "Singh" });
-});
-
-app.post("/user", (req, res) => {
-  //save data in database
-  res.send("Data is save into DB");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("deleted successfully");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Test from server ");
 });
 
 // app.use("/", (req, res) => {
