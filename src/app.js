ffb4d79edb1ff2e1b087ebd119d7174efe0214b3
ajit <ajeets@1028.com>
2024-10-22 17:32:18 +0530

    get,put,post and delete

--- src/app.js
+++ src/app.js
@@ -2,16 +2,27 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/", (req, res) => {
-//   res.send("Home  server");
-// });
-app.use("/Hello", (req, res) => {
-  res.send("Hello  Ajit ");
+//This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Ajit", lastName: "Singh" });
 });
+
+app.post("/user", (req, res) => {
+  //save data in database
+  res.send("Data is save into DB");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("deleted successfully");
+});
+
 app.use("/test", (req, res) => {
   res.send("Test from server ");
 });
 
+// app.use("/", (req, res) => {
+//   res.send("Home from server");
+// });
 app.listen(7777, () => {
   console.log("Server is listening on port 7777");
 });
