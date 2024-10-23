6c417fae93cfa47e1d6588175e37cf1e08e9fb8b
ajit <ajeets@1028.com>
2024-10-23 19:30:58 +0530

    crud

--- src/app.js
+++ src/app.js
@@ -16,6 +16,56 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+//get user by using emailId
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//get all users /feed - get all users from database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//delete user data
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    await User.findByIdAndDelete(userId);
+    res.send("User is deleted");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//update data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate({ _id: userId }, data);
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established...");
