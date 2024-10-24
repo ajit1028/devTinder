1c9f6a47af69715e364a7681b788b8b414fa799b
ajit <ajeets@1028.com>
2024-10-24 10:41:19 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -59,10 +59,12 @@ app.patch("/user", async (req, res) => {
   const userId = req.body.userId;
   const data = req.body;
   try {
-    await User.findByIdAndUpdate({ _id: userId }, data);
+    await User.findByIdAndUpdate({ _id: userId }, data, {
+      runValidators: true,
+    });
     res.send("User updated successfully");
   } catch (error) {
-    res.status(400).send("Something went wrong");
+    res.status(400).send("UPDATE FAILED: " + error.message);
   }
 });
 
