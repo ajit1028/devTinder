c8c46ca9fbcddac4a9d84416ad4acc425e577f6a
ajit <ajeets@1028.com>
2024-12-02 13:17:19 +0530

    userSchema method for jwt and bcrypt in userModel

--- src/app.js
+++ src/app.js
@@ -43,17 +43,11 @@ app.post("/login", async (req, res) => {
     if (!user) {
       throw new Error("Invalid Credentials");
     }
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await user.validatePassword(password);
 
     if (isPasswordValid) {
-      //create JWT token
+      const token = await user.getJWT();
 
-      const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$1092", {
-        expiresIn: "7d",
-      });
-      // console.log(token);
-
-      //add token to cookie and send responce back
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
       });
